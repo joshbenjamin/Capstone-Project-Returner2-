b0cb4b5b11858bcac273bed454bfff776f0fe1c2
Joshua <Joshua@196.24.182.53>
2017-08-31 08:21:58 +0200

    Added the file PageScanner.java

---
+++ Returner2/src/PageScanner.java
@@ -0,0 +1,33 @@
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joshua
+ */
+public class PageScanner
+{
+    private File image;
+    
+    public PageScanner(File image){
+        this.image = image;
+    }
+    
+    public void scan(){
+        
+    }
+    
+    public void orientatePage(){
+        
+    }
+    
+    public void scalePage(){
+        
+    }
+}

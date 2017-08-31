f84dcb6c60b03828faff82c601fc35901eb448e8
Joshua <Joshua@196.24.182.53>
2017-08-31 08:22:15 +0200

    Added the file GenerateReturnerPage.java

---
+++ Returner2/src/GenerateReturnerPage.java
@@ -0,0 +1,28 @@
+
+import java.awt.Image;
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joshua
+ */
+public class GenerateReturnerPage
+{
+    private File image;
+    private Test testDetails;
+    
+    public GenerateReturnerPage(File image, Test testDetails){
+        this.image = image;
+        this.testDetails = testDetails;
+    }
+    
+    public void insertWatermark(){
+        
+    }
+}

e257ee3afd7c101b35507a342abaabcd00dbd129
CBeetar-HP <cameron.beetar@yahoo.co.za>
2017-09-05 21:47:40 +0200

    pdf now accessible

--- Returner2/src/Returner2.java
+++ Returner2/src/Returner2.java
@@ -1,3 +1,10 @@
+
+import java.awt.Desktop;
+import java.io.File;
+
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,7 +19,31 @@ public class Returner2
 {
     public static void main (String[] args)
     {
-        System.out.println("Initializing");
-        
+        try {
+            File pdf = new File("/home/cameron/csc_2016_returner_cover.pdf");
+            if (pdf.exists()) 
+            {
+                //Opens the pdf on the pc but serves no real purpose at this stage.
+                if (Desktop.isDesktopSupported()) 
+                {
+                    //we can open the pdf
+                    Desktop.getDesktop().open(pdf);
+		} 
+                else
+                {
+        		System.out.println("Desktop Error");
+		}
+
+            }
+            else
+            {
+		System.out.println("File not found!");
+            }
+            System.out.println("Done");
+
+	}catch (Exception e) 
+        {
+            e.printStackTrace();
+	}
     }
 }
